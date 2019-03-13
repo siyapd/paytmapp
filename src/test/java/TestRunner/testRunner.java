9b3775b203dc32fe5c3ae7d3774198c2b5959b16
siyapd <siyaprabhudesai292@gmail.com>
2019-03-13 12:56:21 -0700

    used testng annotions instead of junit in testrunner file

--- src/test/java/TestRunner/testRunner.java
+++ src/test/java/TestRunner/testRunner.java
@@ -1,14 +1,16 @@
 package TestRunner;
 
 import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-@RunWith(Cucumber.class)
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+//@RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/Features" , glue="StepDefinations")
 
-
-public class testRunner {
+@Test
+public class testRunner extends AbstractTestNGCucumberTests {
 	
 	
 
